b81302abaf846ee50472e74fcc52917b089bebb2
Daniel Samson <daniel.samson@salesagility.com>
2015-11-25 17:23:05 +0000

    Moving copy vendor code  to  a task

--- Psr/Suite2Psr/Application.php
+++ Psr/Suite2Psr/Application.php
@@ -6,15 +6,14 @@ require_once "Utils.php";
 
 if(!file_exists($APPLICATION_ROOT)) { mkdir($APPLICATION_ROOT, 0777, true); }
 
-/**
- * Copy vendor code
- */
-foreach($VENDOR as $v => $vendor) {
-    $vendor_source_path = $v;
-    $vendor_destination_path = $BUILD_ROOT.DIRECTORY_SEPARATOR.$vendor;
-    if(!file_exists($vendor_destination_path)) { mkdir($vendor_destination_path, 0777, true); }
-    echo $vendor_source_path.PHP_EOL;
-    echo $vendor_destination_path.PHP_EOL;
-    cp($vendor_source_path, $vendor_destination_path);
+
+$tasks = scandir($APPLICATION_TASKS_ROOT);
+
+foreach($tasks as $task) {
+    if($task == '.' || $task == '..') {
+        continue;
+    }
+    require_once $APPLICATION_TASKS_ROOT.$task;
 }
+
 ?>
