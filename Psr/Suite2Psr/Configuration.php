11f47e9d6680c38c49bae0e099649f674a1c26c8
Daniel Samson <daniel.samson@salesagility.com>
2015-11-25 16:39:29 +0000

    Adding SUITECRM_ROOT global

--- Psr/Suite2Psr/Configuration.php
+++ Psr/Suite2Psr/Configuration.php
@@ -1,13 +1,19 @@
 <?php
 echo 'Suite2Psr Configuration'.PHP_EOL;
+global $SUITECRM_ROOT;
+$SUITECRM_ROOT = dirname(dirname(__DIR__));
 
+
+
+echo 'SuiteCRM Source Directory: ';
+echo print_r($suitecrm_root.PHP_EOL, 1);
 /**
  * VENDOR PATHS
  * Old Suite CRM path => new SuiteCRM Path
  * @uses php_inc.php copy_recursive
  */
-global $vendor;
-$vendor = array(
+global $VENDOR;
+$VENDOR = array(
     'include'.DIRECTORY_SEPARATOR.'HTMLPurifier'.DIRECTORY_SEPARATOR => 'vendor'.DIRECTORY_SEPARATOR.'HTMLPurifier'.DIRECTORY_SEPARATOR,
     'include'.DIRECTORY_SEPARATOR.'HTTP_WebDAV_Server'.DIRECTORY_SEPARATOR => 'vendor'.DIRECTORY_SEPARATOR.'HTTP_WebDAV_Server'.DIRECTORY_SEPARATOR,
     'include'.DIRECTORY_SEPARATOR.'nusoap'.DIRECTORY_SEPARATOR => 'vendor'.DIRECTORY_SEPARATOR.'nusoap'.DIRECTORY_SEPARATOR,
@@ -20,4 +26,6 @@ $vendor = array(
     'Zend'.DIRECTORY_SEPARATOR => 'vendor'.DIRECTORY_SEPARATOR.'Zend'.DIRECTORY_SEPARATOR,
     'XTemplate'.DIRECTORY_SEPARATOR => 'vendor'.DIRECTORY_SEPARATOR.'XTemplate'.DIRECTORY_SEPARATOR,
 );
+
+
 ?>
--- Psr/Suite2Psr/Suite2Psr.php
+++ Psr/Suite2Psr/Suite2Psr.php
@@ -1,5 +1,7 @@
 <?php
 echo 'Suite2Psr'.PHP_EOL;
+echo print_r($argv, 1).PHP_EOL;
+
 require("Configuration.php");
 require("Application.php");
 require("Modules.php");
