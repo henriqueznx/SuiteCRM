70180a48977b226222d82c79f5c18b701fad2648
Daniel Samson <daniel.samson@salesagility.com>
2015-11-27 14:52:58 +0000

    Refactored tasks and adding the ability to disable task and patches

--- Psr/Suite2Psr/Application.php
+++ Psr/Suite2Psr/Application.php
@@ -7,15 +7,4 @@ require_once "Utils.php";
 
 if(!file_exists($APPLICATION_ROOT)) { mkdir($APPLICATION_ROOT, 0777, true); }
 
-
-$tasks = scandir($APPLICATION_TASKS_ROOT);
-
-foreach($tasks as $task) {
-    if($task == '.' || $task == '..') {
-        continue;
-    }
-    echo '    '.$task.PHP_EOL;
-    require_once $APPLICATION_TASKS_ROOT.$task;
-}
-
 ?>
--- Psr/Suite2Psr/Suite2Psr.php
+++ Psr/Suite2Psr/Suite2Psr.php
@@ -10,5 +10,33 @@ require("Extension.php");
 if(!file_exists($BUILD_ROOT)) { mkdir($BUILD_ROOT, 0777, true); }
 
 file_put_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'__$MAP_FILES.json', json_encode($MAP_FILES));
+
+echo 'PS4 Tasks'.PHP_EOL;
+
+$tasks = scandir($TASKS_ROOT);
+
+foreach($tasks as $task) {
+    if($task == '.' || $task == '..') {
+        continue;
+    } else if(substr($task, 0, 1) == 'x') {
+        continue;
+    }
+    echo '    '.$task.PHP_EOL;
+    require_once $TASKS_ROOT.$task;
+}
+
+echo 'PSR4 Patches'.PHP_EOL;
+
+$patches = scandir($PATCHES_ROOT);
+
+foreach($patches as $patch) {
+    if($patch == '.' || $patch == '..') {
+        continue;
+    } else if(substr($patch, 0, 1) == 'x') {
+        continue;
+    }
+    echo '    '.$patch.PHP_EOL;
+    require_once $PATCHES_ROOT.$patch;
+}
 die();
 ?>
