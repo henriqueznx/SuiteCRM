9379d6b484d9cdc8ef310c94c812bb0402c114b8
Daniel Samson <daniel.samson@salesagility.com>
2015-11-30 08:21:25 +0000

    edit: 002_copy_modules_files.php

--- Psr/Suite2Psr/PSR4/Tasks/0003_copy_module_files.php
+++ Psr/Suite2Psr/PSR4/Tasks/0003_copy_module_files.php
@@ -1,5 +1,5 @@
 <?php
-global $SUITECRM_ROOT, $module,  $MODULES, $BUILD_ROOT, $module_ROOT, $MODULES_ROOT, $EXTENSION_ROOT, $module_TASKS_ROOT, $SUITE2PSR_ROOT;
+global $SUITECRM_ROOT, $module,  $MODULES, $BUILD_ROOT, $MODULES_ROOT, $MODULES_ROOT, $EXTENSION_ROOT, $module_TASKS_ROOT, $SUITE2PSR_ROOT;
 /**
  * Copy application code
  */
@@ -7,7 +7,7 @@ foreach($MODULES as $m => $module) {
     $module_source_path = $SUITECRM_ROOT.$m;
 //    $module_source_path = realpath($SUITECRM_ROOT.$m);
 
-    $module_destination_path = $module_ROOT.$module;
+    $module_destination_path = $MODULES_ROOT.$module;
     if(!file_exists($module_destination_path) && is_dir($module_source_path)) { mkdir($module_destination_path, 0777, true); }
 //    $module_destination_path = realpath($module_ROOT.$module);
     $module_destination_path = str_replace(DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR,'/',$module_destination_path);
