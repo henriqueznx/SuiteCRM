12bd0e9858b0469a824668184c3b0725c9327a08
paulgodley <paul.godley@salesagility.com>
2016-05-11 15:59:23 +0100

    Updated to maintain a list of the checked filter items

---
+++ testSearch.php
@@ -0,0 +1,30 @@
+<?php
+if(!defined('sugarEntry'))define('sugarEntry', true);
+
+require_once('include/entryPoint.php');
+//require_once('include/utils/file_utils.php');
+
+
+//$test = BeanFactory::getBean('Accounts');
+
+$return_array= Array();
+
+$db = DBManagerFactory::getInstance();
+$query="select distinct name, id from accounts where name like'%$_REQUEST[query]%' and deleted=0";
+$result=$db->query($query);
+if (!empty($result)) {
+    while (($row=$db->fetchByAssoc($result)) != null) {
+        $std = new stdClass();
+        $std->value = $row['name'];
+        $std->data = $row['id'];
+        $return_array[]=$std;
+    }
+}
+$t = new stdClass();
+$t->suggestions = $return_array;
+echo json_encode($t);
+
+//echo $_REQUEST[query];
+
+
+
